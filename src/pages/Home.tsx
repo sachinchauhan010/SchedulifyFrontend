89375700cfd471d9fba68c10acc66cbe33867246
alokverma <alokverma749@gmail.com>
2024-09-19 18:52:32 +0530

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,17 @@ import FileUpload from "@/components/FileUpload"
 import ScheduleTable from "@/components/ScheduleTable"
 
 function Home() {
+  const handleCheck = async () => {
+
+    console.log('hello')
+    await fetch(`${import.meta.env.VITE_PRODUCTION_URI}/api/faculty/faculty-get`, { credentials: 'include' })
+  }
+
   return (
     <div>
-      <FileUpload/>
-      <ScheduleTable/>
+      <button className="border border-red-900 p-3 m-3" onClick={handleCheck}>check Auth</button>
+      <FileUpload />
+      <ScheduleTable />
     </div>
   )
 }
