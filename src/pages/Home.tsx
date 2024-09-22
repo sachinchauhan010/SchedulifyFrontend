3d623c06bec2e73337327cc9ccbf17120fcf34d5
sachinchauhan010 <010sssachin@gmail.com>
2024-09-22 21:32:22 +0530

    chart added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,9 @@
-import ScheduleTable from "@/components/ScheduleTable"
+import { ClassChart } from "@/components/ClassChart"
 
 function Home() {
-  // const handleCheck = async () => {
-  //   await fetch(`${import.meta.env.VITE_PRODUCTION_URI}/api/faculty/faculty-get`, { credentials: 'include' })
-  // }
-
   return (
     <div>
-      {/* <button className="border border-red-900 p-3 m-3" onClick={handleCheck}>check Auth</button> */}
-      <ScheduleTable />
+      <ClassChart/>
     </div>
   )
 }
